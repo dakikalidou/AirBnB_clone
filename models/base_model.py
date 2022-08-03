be0df6a78fd7c39838a57db673c3d5f74a7b3570
Boakye2 <99751184+Boakye2@users.noreply.github.com>
2022-08-03 13:39:31 +0200

    class BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -14,14 +14,14 @@ class BaseModel:
             *args (any): won’t be used.
             **kwargs (dict): Key/value pairs of attributes.
         """
-        tform = "%Y-%m-%dT%H:%M:%S.%f"
+        tformat = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
         if len(kwargs) != 0:
             for k, v in kwargs.items():
                 if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] = datetime.strptime(v, tform)
+                    self.__dict__[k] = datetime.strptime(v, tformat)
                 else:
                     self.__dict__[k] = v
         else:
